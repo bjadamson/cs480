7e210544c8f56732c03acc423aa72315ab6a9dc0
kos.wesley <kos.wesley@gmail.com>
2012-03-12 18:59:01 -0700

    derp

--- Compiler/Tree/TreeNode.cs
+++ Compiler/Tree/TreeNode.cs
@@ -6,6 +6,7 @@ using Tokens;
 
 namespace Compiler.Tree
 {
+<<<<<<< .merge_file_a09440
 	public class TreeNode
 	{
 		public TreeNode LeftChild { get; set; }
@@ -49,4 +50,54 @@ namespace Compiler.Tree
 			}
 		}
 	}
+=======
+    public class TreeNode
+    {
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+        public TreeNode Parent { get; set; }
+
+        public Token Token;
+
+        public TreeNode(Token token) {
+            Token = token;
+        }
+
+        public void SetParent(TreeNode parent) {
+            Parent = parent;
+        }
+
+        public void SetLeftChild(Token token) {
+            LeftChild = new TreeNode(token);
+            LeftChild.SetParent(this);
+        }
+
+        public void SetRightChild(Token token) {
+            RightChild = new TreeNode(token);
+            RightChild.SetParent(this);
+        }
+
+        public bool HasLeftChild() {
+            return LeftChild != null;
+        }
+
+        public bool HasRightChild() {
+            return RightChild != null;
+        }
+
+        public bool CorrectNumberChildren() {
+            if (Token.Type == TokenType.Operator && Token.Key == "not") {
+                return HasLeftChild();
+            }
+            else {
+                return HasLeftChild() && HasRightChild();
+            }
+        }
+
+        public void FormatStringConcat() {
+            Token.Key = Token.Key.Substring(1, Token.Key.Length - 2);
+            Parent.RightChild.Token.Key = Parent.RightChild.Token.Key.Substring(1, Parent.RightChild.Token.Key.Length - 1);
+        }
+    }
+>>>>>>> .merge_file_a08132
 }
--- Compiler/Tree/TreeStructure.cs
+++ Compiler/Tree/TreeStructure.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Tokens;
 
+<<<<<<< .merge_file_a06204
 namespace Compiler.Tree
 {
 	public class TreeStructure
@@ -91,4 +92,132 @@ namespace Compiler.Tree
 			Console.WriteLine();
 		}
 	}
+=======
+namespace Compiler.Tree {
+    public class TreeStructure {
+        private List<string> floatingPointOperators = new List<string>() {
+            "sin", "tan", "cos", "exp", 
+        };
+
+        TreeNode rootNode = null;
+        TreeNode sentinel = null;
+        int needsConcat = 0;
+        string stringConcat = "";
+
+        public void AddToken(Token token) {
+            if (rootNode == null) {
+                rootNode = new TreeNode(token);
+                sentinel = rootNode;
+            }
+            else {
+                while (sentinel.CorrectNumberChildren()) {
+                    sentinel = sentinel.Parent;
+                }
+                if (token.Type != TokenType.Operator) {
+                    Insert(token, sentinel);
+                }
+                else {
+                    InsertOperator(token, sentinel);
+                }
+            }
+        }
+
+        private void InsertOperator(Token token, TreeNode node) {
+            Insert(token, node);
+            sentinel = node.HasRightChild()
+                ? node.RightChild
+                : node.LeftChild;
+        }
+
+        private void Insert(Token token, TreeNode node) {
+            if (node.HasLeftChild()) {
+                node.SetRightChild(token);
+            }
+            else {
+                node.SetLeftChild(token);
+            }
+        }
+
+        private void PrintTreePostTraversalHelper(TreeNode node) {
+            if (node == null) {
+                return;
+            }
+
+            PrintTreePostTraversalHelper(node.LeftChild);
+            PrintTreePostTraversalHelper(node.RightChild);
+
+            if (node.Token.Type == TokenType.String) {
+                if (node.Parent != null
+                    && node.Parent.RightChild != null
+                    && node.Parent.Token.Key == "+") {
+                        if (node.Parent.RightChild.Token.Type != TokenType.String) {
+                            stringConcat = stringConcat == string.Empty ? node.Token.Key.Substring(1, node.Token.Key.Length - 2) :
+                                                            stringConcat + " " + node.Token.Key.Substring(1, node.Token.Key.Length - 2);
+                            ++needsConcat;
+                        }
+                        else { 
+                            node.FormatStringConcat();
+                            Console.Write("s\" {0}{1}{2} {3}", stringConcat, stringConcat == string.Empty ? "" : " ", node.Token.Key, node.Parent.RightChild.Token.Key);
+                            node.Parent.RightChild = null;
+                            ++needsConcat;
+                        }
+                }
+                else {
+                    Console.Write("s\" " + node.Token.Key.Substring(1));
+                }
+            }
+            else if (node.Token.Type == TokenType.Real) {
+                Console.Write(node.Token.Key + "e");
+            }
+            else if (node.Token.Type == TokenType.Operator) {
+                if (floatingPointOperators.Contains(node.Token.Key)) {
+                    Console.Write("f" + node.Token.Key);
+                }
+                else if (node.Token.Key == "iff") {
+                    Console.Write("invert xor");
+                }
+                else if (node.Token.Key == "not") {
+                    Console.Write("invert");
+                }
+                else if (needsConcat > 0) {
+                    // A string concat has occured and we reset the flag and don't print out the +
+                    --needsConcat;
+                }
+                else if (node.LeftChild.Token.Type == TokenType.Real && node.RightChild.Token.Type == TokenType.Real) {
+                    Console.Write("f" + node.Token.Key);
+                }
+                else if (node.RightChild == null && node.Token.Key == "-") {
+                    if (node.LeftChild.Token.Type == TokenType.Real) {
+                        Console.Write("fnegate");
+                    }
+                    else if (node.LeftChild.Token.Type == TokenType.Integer) {
+                        Console.Write("negate");
+                    }
+                }
+                else if (node.Token.Key == "%") {
+                    if (node.LeftChild.Token.Type == TokenType.Real && node.RightChild.Token.Type == TokenType.Real) {
+                        Console.Write("fmod");
+                    }
+                    else if (node.LeftChild.Token.Type == TokenType.Real && node.RightChild.Token.Type == TokenType.Real) {
+                        Console.Write("mod");
+                    }
+                }
+                else {
+                    Console.Write(node.Token.Key);
+                }
+            }
+            else {
+                Console.Write(node.Token.Key);
+            }
+            if (needsConcat == 0) { 
+                Console.Write(" ");
+            }
+        }
+
+        public void PrintTreePostTraversal() {
+            PrintTreePostTraversalHelper(rootNode);
+            Console.WriteLine();
+        }
+    }
+>>>>>>> .merge_file_a06384
 }
